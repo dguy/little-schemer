d0331006ac9e22ca0c0dc0ccd4ba52e61e1b1466
Damian Guy <damian.guy@gmail.com>
2012-08-04 12:36:45 +0100

    remove

--- numbers.rkt
+++ numbers.rkt
@@ -83,3 +83,11 @@
     (cond
       [(zero? (sub1 n)) (car lat)]
       [else (pick (sub1 n) (cdr lat))])))
+
+(define remove
+  (lambda (n lat)
+    (cond
+      [(zero? (sub1 n)) (cdr lat)]
+      [else (cons (car lat) (remove (sub1 n) (cdr lat)))])))
+
+
