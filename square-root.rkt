27227f9c787faff71a2a08ddd73b2058b9f974dc
Damian Guy <damian.guy@gmail.com>
2012-08-05 17:31:25 +0100

    square-root

---
+++ square-root.rkt
@@ -0,0 +1,18 @@
+#lang racket
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (square x) (* x x))
+
+(define (sqrt x)
+ (define (try guess)
+  (if (good-enough? guess)
+    guess
+    (try (improve guess))))
+ (define (improve guess)
+    (average guess (/ x guess)))
+ (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.00000001))
+ (try 1.0)) 
+
+
