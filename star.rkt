c1869dfe2ac16b168ccf3ae18c771f0c4314d3e4
Damian Guy <damian.guy@gmail.com>
2012-08-07 10:29:42 +0100

    occur*

--- star.rkt
+++ star.rkt
@@ -1,17 +1,17 @@
 #lang racket
-(require "lat.rkt")
+(require "lat.rkt" "eqan.rkt")
 
-(provide rember* insert-after*)
+(provide remove-member* insert-after* occur*)
 
-(define (rember* a l)
+(define (remove-member* a l)
   (cond
     [(null? l) (list)]
     [(atom? (car l))
      (cond
-        [(eq? a (car l)) (rember* a (cdr l))]
-        [else (cons (car l) (rember* a (cdr l)))])
+        [(eq? a (car l)) (remove-member* a (cdr l))]
+        [else (cons (car l) (remove-member* a (cdr l)))])
        ]
-     [else (cons (rember* a (car l)) (rember* a (cdr l)))]))
+     [else (cons (remove-member* a (car l)) (remove-member* a (cdr l)))]))
 
 
 (define (insert-after* new old l)
@@ -22,3 +22,13 @@
        [(eq? old (car l)) (cons old (cons new (insert-after* new old (cdr l))))]
        [else (cons (car l) (insert-after* new old (cdr l)))])]
     [else (cons (insert-after* new old (car l)) (insert-after* new old (cdr l)))]))
+
+(define (occur* value l)
+  (cond
+    [(null? l) 0]
+    [(atom? (car l))
+     (cond
+       [(eqan? value (car l)) (add1 (occur* value (cdr l)))]
+       [else (occur* value (cdr l))])]
+    [else (+ (occur* value (car l)) (occur* value (cdr l)))]))
+       
