a83b9957af2a3bdeee4736937f861c78c5f9df91
Damian Guy <damian.guy@gmail.com>
2012-08-27 16:52:25 +0100

    rational

---
+++ sicp/ch2/rational.rkt
@@ -0,0 +1,40 @@
+#lang racket
+
+(require "../ch1/euclid.rkt")
+(provide make-rat numer denom add-rat sub-rat mul-rat div-rat equal-rat? print-rat)
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
+
+(define numer car)
+(define denom cdr)
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (displayln (denom x)))
